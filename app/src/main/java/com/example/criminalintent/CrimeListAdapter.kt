daecf02390685e3fe55593e6823c953dbf64e446
adamcdaypro <acday.pro@pm.me>
2023-05-10 09:26:20 -0700

    BNRG Chapter 11 - Move to Constraint Layouts - Format date/time

--- app/src/main/java/com/example/criminalintent/CrimeListAdapter.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListAdapter.kt
@@ -1,18 +1,21 @@
 package com.example.criminalintent
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.criminalintent.databinding.ListItemCrimeBinding
 import com.example.criminalintent.model.Crime
 import com.google.android.material.snackbar.Snackbar
+import java.text.DateFormat
 
 class CrimeListAdapter(
     private val crimes: List<Crime>
 ) : RecyclerView.Adapter<CrimeListAdapter.CrimeViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeViewHolder {
-        val binding = ListItemCrimeBinding.inflate(LayoutInflater.from(parent.context))
+        val binding =
+            ListItemCrimeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return CrimeViewHolder(binding)
     }
 
@@ -22,12 +25,18 @@ class CrimeListAdapter(
         holder.bind(crimes[position])
     }
 
-    class CrimeViewHolder(private val binding: ListItemCrimeBinding) : RecyclerView.ViewHolder(binding.root) {
+    class CrimeViewHolder(private val binding: ListItemCrimeBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
         fun bind(crime: Crime) {
-            with (binding) {
+            with(binding) {
                 crimeTitleTextView.text = crime.title
-                crimeDateTextView.text = crime.date.toString()
+                val date = DateFormat.getDateTimeInstance(
+                    DateFormat.FULL, DateFormat.SHORT
+                ).format(crime.date)
+                crimeDateTextView.text = date
+
+                crimeSolvedImageView.visibility = if (crime.isSolved) View.VISIBLE else View.GONE
 
                 root.setOnClickListener {
                     Snackbar.make(it, "${crime.title} clicked", Snackbar.LENGTH_SHORT).show()
